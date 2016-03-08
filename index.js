29c2faa298e5c94b871ff9ec21ccefc25c56c9df
Kevin Stumpf <kevin@dispatchertrucking.com>
2016-03-07 19:24:06 -0800

    Added dummy for Android

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-var { NativeModules } = require('react-native');
+var { NativeModules, Platform } = require('react-native');
 
-var rnSegment = NativeModules.RNSegment;
+var rnSegment = Platform.OS === 'ios' ? NativeModules.RNSegment : null;
 
 class Segment {
   setupWithConfiguration = (writeKey, debug = false) => {
+    if(!rnSegment) {
+      return;
+    }
+
     rnSegment.setupWithConfiguration(writeKey, debug);
   };
 
   identify = (userId, traits = {}, options = {}) => {
+    if(!rnSegment) {
+      return;
+    }
+
     rnSegment.identify(userId, traits, options);
   };
 
   track = (event, properties = {}, options = {}) => {
+    if(!rnSegment) {
+      return;
+    }
+
     rnSegment.track(event, properties, options);
   };
 
   screen = (event, properties = {}, options = {}) => {
+    if(!rnSegment) {
+      return;
+    }
+
     rnSegment.screen(event, properties, options);
   };
 
   alias = (newid, options = {}) => {
+    if(!rnSegment) {
+      return;
+    }
+
     rnSegment.alias(newId, options);
   };
 
   reset = () => {
+    if(!rnSegment) {
+      return;
+    }
+    
     rnSegment.reset();
   };
 }
